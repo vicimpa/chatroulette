53f1a81be1d0d3f063f6dc7a3458f1df5c6fa818
Илья <iltrucco@mail.ru>
2021-06-22 07:37:20 +0300

    Add skit and screen by keypress a/s

--- dist/preload.js
+++ dist/preload.js
@@ -1,6 +1,17 @@
+// ==UserScript==
+// @name         ChatRoulette Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://videochatru.com/*
+// @icon         https://www.google.com/s2/favicons?domain=videochatru.com
+// @grant        none
+// ==/UserScript==
+
 Object.defineProperty(window, 'FCN', {
   get() {
-    function r() {}
+    function r() { }
 
     r.isValid = function (e) {
       return true
@@ -16,7 +27,7 @@ Object.defineProperty(window, 'FCN', {
   }
 })
 let state = []
-let func = () => {}
+let func = () => { }
 let first = true
 
 Object.defineProperty(window, '_0x3be7', {
@@ -35,12 +46,12 @@ Object.defineProperty(window, '_0x5138', {
   },
   set(v) {
     func = v
-    if(first) {
+    if (first) {
       first = false
       _0x3be7 = state.map((e, i) => {
         let d = func(i)
-        
-        if(/Snap Camera/.test(d))
+
+        if (/Snap Camera/.test(d))
           return 'asdfasjdcnawecaseaec'
         else
           return d
@@ -52,10 +63,10 @@ Object.defineProperty(window, '_0x5138', {
 
 
 function toObject(from = {}, to = {}) {
-  for(let key in from) {
+  for (let key in from) {
     let value = from[key]
 
-    if(typeof value === 'object' && value && !Array.isArray(value)) {
+    if (typeof value === 'object' && value && !Array.isArray(value)) {
       toObject(value, from[key])
       continue
     }
@@ -65,19 +76,23 @@ function toObject(from = {}, to = {}) {
 }
 
 /**
- * @param {string} tagName 
- * @param {Partial<HTMLElement> & {ref(v: HTMLDivElement) => void}} options 
- * @param {HTMLElement[]} childs 
- */
+* @param {string} tagName
+* @param {Partial<HTMLElement> & {ref(v: HTMLDivElement) => void}} options
+* @param {HTMLElement[]} childs
+*/
 function createElement(tagName = '', options = {}, childs = []) {
   const element = document.createElement(tagName)
+  const {style = {}, ...other} = options
 
-  toObject(options, element)
+  toObject(other, element)
 
-  for(let child of childs)
+  for(let key in style)
+    element.style[key] = style[key]
+
+  for (let child of childs)
     element.appendChild(child)
 
-  if(typeof options.ref == 'function')
+  if (typeof options.ref == 'function')
     options.ref(element)
 
   return element
@@ -90,10 +105,10 @@ async function runDevices(/** @type {HTMLDivElement} */ frame) {
   const outputId = 'output_device'
   const state = {
     _needDevice: '',
-    /** @type {HTMLDivElement} */ labelDevice: null,
-    /** @type {HTMLDivElement} */mediaSelect: null,
+/** @type {HTMLDivElement} */ labelDevice: null,
+/** @type {HTMLDivElement} */mediaSelect: null,
     get needDevice() {
-      if(this._needDevice)
+      if (this._needDevice)
         return outputDevices.find(e => e.deviceId == this._needDevice) ?
           this._needDevice : 'default'
 
@@ -103,12 +118,12 @@ async function runDevices(/** @type {HTMLDivElement} */ frame) {
       localStorage.setItem(outputId, v)
       this._needDevice = v
 
-      this.labelDevice.innerText = 
-        outputDevices.find(e => 
+      this.labelDevice.innerText =
+        outputDevices.find(e =>
           e.deviceId == this.needDevice).label
 
-      if(outputDevices.length > 1)
-          this.mediaSelect.style.display = 'block'
+      if (outputDevices.length > 1)
+        this.mediaSelect.style.display = 'block'
 
       setTimeout(() => hide(), 1)
     }
@@ -125,14 +140,14 @@ async function runDevices(/** @type {HTMLDivElement} */ frame) {
     state.mediaSelect.querySelector('.media-select__list')
       .style.display = 'none'
   }
-    
+
   const mediaSelect = createElement('div', {
     className: 'media-select',
     id: 'audio-devices-out',
     ref: (e) => state.mediaSelect = e,
     onclick: () => show(),
     onmouseleave: () => hide(),
-    style: {display: 'block'}
+    style: { display: 'block' }
   }, [
     createElement('div', {
       className: 'media-select__select'
@@ -178,40 +193,106 @@ async function runDevices(/** @type {HTMLDivElement} */ frame) {
     /** @type {HTMLVideoElement} */
     const videoContainer = document.getElementById('remote-video')
 
-    if(!videoContainer)
+    if (!videoContainer)
       return null
-    
-    if(videoContainer.sinkId == deviceId)
+
+    if (videoContainer.sinkId == deviceId)
       return null
 
     videoContainer.setSinkId(deviceId)
-      .catch(() => {})
+      .catch(() => { })
   }, 200)
 }
 
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')
+
+const screenShot = () => {
+  const remoteVideo = document.getElementById('remote-video')
+
+  if(remoteVideo instanceof HTMLVideoElement) {
+    const {videoWidth, videoHeight} = remoteVideo
+
+    if(!videoWidth || !videoHeight)
+      return
+
+    canvas.width = videoWidth
+    canvas.height = videoHeight
+    ctx.drawImage(remoteVideo, 0, 0)
+
+    const url = canvas.toDataURL('jpeg')
+    const a = document.createElement('a')
+
+    a.download = 'screen.jpg'
+    a.href = url
+    console.log(url)
+    a.click()
+  }
+}
+
+addEventListener('keydown', ({key, target}) => {
+  if(target.contentEditable == 'true') return
+
+  switch(key) {
+    case 's':
+    case 'S':
+      screenShot()
+      break
+
+    case 'a':
+    case 'A':
+      document.querySelector('.start-button .btn__bg')?.click()
+      break
+
+    default:
+      console.log('Key down', key)
+  }
+})
+
 let interval = setInterval(() => {
+  /** @type {HTMLDivElement} */
+  const remoteVido = document.getElementById('remote-video-wrapper')
+
+  if(!remoteVido)
+    return null
+
+  createElement('button', {
+    style: {
+      position: 'absolute',
+      top: '20px',
+      left: '20px',
+      zIndex: 99
+    },
+    innerText: 'Скриншот',
+    onclick: screenShot,
+    ref(v) {
+      remoteVido.appendChild(v)
+    }
+  })
+
+
   /** @type {HTMLDivElement} */
   const localVideo = document.getElementById('local-video-wrapper')
 
-  if(!localVideo) 
+  if (!localVideo)
     return null
 
   /** @type {HTMLDivElement} */
   const mediaDevices = localVideo.querySelector('#media-devices')
 
-  if(!mediaDevices)
+  if (!mediaDevices)
     return null
 
   /** @type {HTMLDivElement} */
   const frame = mediaDevices.querySelector('.media-devices__frame')
 
-  if(!frame)
+  if (!frame)
     return null
 
   /** @type {HTMLDivElement} */
   const wrapper = mediaDevices.querySelector('.media-devices__wrapper')
 
-  if(!wrapper)
+  if (!wrapper)
     return null
 
   clearInterval(interval)
